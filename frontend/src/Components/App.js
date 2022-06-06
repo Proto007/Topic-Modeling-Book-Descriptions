8c93168e725a8621050f6a0e465b5beed22e6daf
Yahya Yusuf <webyahyaver@gmail.com>
2022-06-06 02:31:16 -0500

    Remove probabilities

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -8,12 +8,11 @@ function App() {
   const [retrieved, setRetrieved] = useState(false);
   // Topics object that maps (topic -> probability)
   const [topics, setTopics] = useState(null);
-  const [probabilities, setProbabilities] = useState(null);
   
   // If data is available, then render results; otherwise, render form
   return (
     <div className="App">
-      {retrieved ? <Results topics={topics} probabilities={probabilities}/> : <DescriptionInput setRetrieved = {setRetrieved} setTopics = {setTopics} setProbabilities = {setProbabilities}/>}
+      {retrieved ? <Results topics={topics}/> : <DescriptionInput setRetrieved = {setRetrieved} setTopics = {setTopics}/>}
     </div>
   );
 }
