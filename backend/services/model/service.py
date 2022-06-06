4f5f8407ce48eb223809d0803dc6610c555d6f64
Yahya Yusuf <webyahyaver@gmail.com>
2022-06-06 02:34:48 -0500

    Train model in service.py. Use predict method.

--- backend/services/model/service.py
+++ backend/services/model/service.py
@@ -1,18 +1,20 @@
 import numpy as np
-import ldaModel
 import bentoml
+import ldaModel
+from bentoml.io import NumpyNdarray, Text
 
-from bentoml.io import NumpyNdarray
 
+data_vectorized,vectorizer=ldaModel.get_data_vectorized('books.csv',10, 0.8)
+lda_model=ldaModel.create_lda_model(data_vectorized,15)
+bentoml.sklearn.save_model("ldaModel", lda_model)
+df_topic_keywords = ldaModel.show_topics(vectorizer, lda_model,15)
 
-data_vectorized,vectorizer=ldaModel.get_data_vectorized('books.csv',10)
-lda_model=ldaModel.create_lda_model(data_vectorized,20)
-bentoml.sklearn.save_model("lda_model", lda_model)
-
-lda_model_runner = bentoml.sklearn.get("lda_model:latest").to_runner()
-
+lda_model_runner = bentoml.sklearn.get("ldaModel:latest").to_runner()
 svc = bentoml.Service("lda_model_classifier", runners=[lda_model_runner])
 
-@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
-def classify(input_series: np.ndarray) -> np.ndarray:
-    return lda_model_runner.predict.run(input_series)
+@svc.api(input=Text(), output=NumpyNdarray())
+def predict(input_series) -> np.ndarray:
+    topics = ldaModel.predict(input_series, vectorizer, lda_model, df_topic_keywords) 
+    print(topics)
+    topicsArr = np.array(topics)
+    return topicsArr
