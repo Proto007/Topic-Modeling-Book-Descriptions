a102cf96e6193b27b4eedbba63eb134111ba0692
Yahya Yusuf <webyahyaver@gmail.com>
2022-06-06 05:03:00 -0500

    Add fetch instead of axios. Add bento_config.yaml in an attempt to fix cors issue.

--- frontend/src/Components/DescriptionInput.js
+++ frontend/src/Components/DescriptionInput.js
@@ -1,21 +1,20 @@
 import { useState } from "react";
-import axios from 'axios';
 
 // Component that parses user input and sends POST request to backend
 const DescriptionInput = (props) => {
     const [description, setDescription] = useState('');
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const response = await axios.post(`http://127.0.0.1:5000/predict`, {
-            desc: description
-        }, {
+        const response = await fetch(`http://0.0.0.0:5000/predict`, {
+            method: 'POST', 
             headers: {
-                'Content-Type': 'text/plain;charset=utf-8',
-                "Access-Control-Allow-Origin": "*",
-            }
-        });
-       props.setRetrieved(true);
-       props.setTopics([].concat(response.data.topics));
+              'Content-Type': 'text/plain;charset=utf-8'
+            },
+            body: description
+          })
+        console.log(response);
+        props.setTopics([].concat(response.body.topics));
+        props.setRetrieved(true);
     }
     // Render book description form
     return (
