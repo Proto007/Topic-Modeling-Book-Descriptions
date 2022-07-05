84baa3ae2f823080cab1c5f672ababe4314deaa1
Sadab Hafiz <sadabhafizny@gmail.com>
2022-07-05 15:32:26 -0400

    Get arrays of keywords and topic correlations

--- frontend/src/Components/DescriptionInput.js
+++ frontend/src/Components/DescriptionInput.js
@@ -1,22 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Introduction from "./Introduction";
 
 // Component that parses user input and sends POST request to backend
 const DescriptionInput = (props) => {
     const [description, setDescription] = useState('');
+    const [prediction, setPrediction] = useState([]);
+    const [retrieved, setRetrieved] = useState(false);
+    const [topicWords, setTopicWords]= useState([]);
+    const [correlations, setCorrelations]= useState([]);
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const response = await fetch(`http://127.0.0.1:3000/predict`, {
+        const response = await fetch(`http://127.0.0.1:8000/http://127.0.0.1:3000/predict`, {
             method: 'POST', 
             headers: {
               'Content-Type': 'text/plain;charset=utf-8'
             },
             body: description
           })
-        console.log(response);
-        props.setTopics([].concat(response.body.topics));
-        props.setRetrieved(true);
+        let body = await response.json(); 
+        setPrediction([].concat(body));
+        setRetrieved(true);
     }
+    useEffect(()=>{
+      let keywords_arr=[];
+      let correlations_arr=[]; 
+      prediction.map((val)=>{
+        keywords_arr.push(val[0]);
+        correlations_arr.push(val[1]);
+      });
+      setTopicWords(keywords_arr);
+      setCorrelations(correlations_arr);
+    },[prediction])
+    
     // Render book description form
     return (
      <div style={ {
@@ -25,7 +40,7 @@ const DescriptionInput = (props) => {
       flexDirection: "row",
       justifyContent:"flex-start"
     }}>
-         <Introduction/>
+         <Introduction retrieved={retrieved} topicWords={topicWords} correlations={correlations}/>
          <form onSubmit={handleSubmit}>
             <textarea placeholder="Enter a book description..." rows="28" cols="60" value={description} onChange={(event) => setDescription(event.target.value)} style={{fontSize:"20pt",marginLeft:"2px"}}/> <br/>
             <input type='submit' value='Get Topics' style={{width:'95vh',height:'6.8vh', background:"#000000",color:"white", marginLeft:"2px",fontSize:"20pt", fontFamily:"cursive",padding:"10px 25px",cursor:"pointer"}}/> 
